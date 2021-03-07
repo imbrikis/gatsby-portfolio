1c6994d10ebea7e307a12e27878a41f01305dd94
imbrikis <imbrikis@yahoo.com>
2021-03-06 23:00:10 -0700

    sidebar toggle functionality

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 // import "../css/main.css"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(true)
+  const toggleSidebar = () => {
+    setSidebarIsOpen(!sidebarIsOpen)
+  }
+
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar toggleSidebar={toggleSidebar} isOpen={sidebarIsOpen} />
       {children}
       <Footer />
     </>
