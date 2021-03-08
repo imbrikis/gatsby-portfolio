898701884f4c9896c1d85ab1d8721ea20965bea4
imbrikis <imbrikis@yahoo.com>
2021-03-08 10:11:00 -0700

    styling: about page (link from Strapi)

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,62 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>About page</h2>
+
+const About = ({
+  data: {
+    allStrapiAbout: { nodes },
+  },
+}) => {
+  const {
+    title,
+    info,
+    stack,
+    image: {
+      childImageSharp: { fluid },
+    },
+  } = nodes[0]
+
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map(item => {
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiAbout {
+      nodes {
+        id
+        title
+        info
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default About
