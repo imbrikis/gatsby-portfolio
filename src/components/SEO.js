434c599b5078408dc9b7d140f9b5f073be5845fd
imbrikis <imbrikis@yahoo.com>
2021-03-08 13:05:07 -0700

    add Twitter cards to SEO comp

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -6,7 +6,6 @@ const query = graphql`
   {
     site {
       siteMetadata {
-        author
         siteDescription: description
         image
         siteUrl
@@ -25,13 +24,17 @@ const SEO = ({ title, description }) => {
     siteUrl,
     image,
     twitterUsername,
-    author,
   } = site.siteMetadata
 
   return (
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDescription} />
       <meta name="image" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDescription} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
